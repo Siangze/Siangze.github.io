219ca1f2e765a4ba4366e5dc109253f873f8644a
Siangze <167446262+Siangze@users.noreply.github.com>
2024-11-02 11:52:04 +0800

    update2

--- Application.js
+++ Application.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded',function(){
     const menuItems = document.querySelectorAll('.menu-item');
-    menuItems.forEach(item => {
-        //close all other open items
-        item.addEventListener('click',() => {
-            menuItems.forEach(otheritem => {
-                if (otheritem !== item){
-                    otheritem.classList.remove('active');
-                }
+        menuItems.forEach(item => {
+        const menu = item.querySelector('.desktop-menu');
+                //add both click and touch events
+            ['click', 'touched'].forEach(eventType=>{
+                menu.addEventListener(eventType,function(e){
+                e.preventDefault();
+                //close all other open items
+                menuItems.forEach(otheritem => {
+                    if (otheritem !== item){
+                        otheritem.classList.remove('active');
+                    }
+                });
+                //Toggle switch
+                item.classList.toggle('active');
             });
-            //Toggle switch
-            item.classList.toggle('active');
         });
     });
 });
