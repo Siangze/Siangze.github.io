b3b31541f38dc70d014c898ed9151aa8b65c6c69
Siangze <167446262+Siangze@users.noreply.github.com>
2024-11-02 12:13:50 +0800

    update3

--- Application.js
+++ Application.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded',function(){
-    const menuItems = document.querySelectorAll('.menu-item');
+    const menuItems = document.querySelectorAll('.menu-item'); //store elements with class 'menu-item'
         menuItems.forEach(item => {
-        const menu = item.querySelector('.desktop-menu');
+        const menu = item.querySelector('.desktop-menu'); //find the element: desktop-menu
                 //add both click and touch events
             ['click', 'touched'].forEach(eventType=>{
                 menu.addEventListener(eventType,function(e){
-                e.preventDefault();
+                e.preventDefault(); //prevent default browser behavior (scrolling on touch)
                 //close all other open items
                 menuItems.forEach(otheritem => {
                     if (otheritem !== item){
                         otheritem.classList.remove('active');
                     }
                 });
-                //Toggle switch
+                //Toggle the active class on the clicked item: if it's active then remove it; if it's not active then add it
                 item.classList.toggle('active');
             });
         });
